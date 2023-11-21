bd6203dfbc08c22be2734fed23f2b57e26644aee
Gustav Sundin <sundin666@gmail.com>
2023-11-21 14:46:39 +0100

    vuex-persist

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueGtag from 'vue-gtag-next';
 // eslint-disable-next-line import/no-unresolved
 import { Amplify } from 'aws-amplify';
 import { createStore } from 'vuex';
+import VuexPersistence from 'vuex-persist'
 import App from './App.vue';
 import router from './router';
 
@@ -11,6 +12,10 @@ Amplify.configure({
   aws_cognito_region: 'eu-north-1',
   aws_user_pools_id: 'eu-north-1_wScBt5dG1',
   aws_user_pools_web_client_id: '6rji8tci1r0u5mfdp8iemtvjmn',
+});
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
 });
 
 const store = createStore({
@@ -28,6 +33,7 @@ const store = createStore({
       state.shoppingCart.push(product);
     }
   },
+  plugins: [vuexLocal.plugin],
 });
 
 createApp(App)
