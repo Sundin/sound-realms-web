f2a16eaaedfbfa0fbbbacc6bd839340be48079da
Gustav Sundin <sundin666@gmail.com>
2023-11-28 13:15:45 +0100

    KS link

--- src/adventures/dracula/Dracula.vue
+++ src/adventures/dracula/Dracula.vue
@@ -3,16 +3,16 @@
     <div class="content">
       <TopMenu />
       <MyBreadcrumbs
-            :breadcrumbs="[
-              {
-                title: 'Sound Realms',
-                link: '/'
-              },
-              {
-                title: 'Dracula: Curse of the Vampire'
-              },
-            ]"
-          />
+        :breadcrumbs="[
+          {
+            title: 'Sound Realms',
+            link: '/',
+          },
+          {
+            title: 'Dracula: Curse of the Vampire',
+          },
+        ]"
+      />
       <div class="section-content">
         <img
           src="../../assets/dracula/dracula-hero-image.png"
@@ -21,20 +21,35 @@
         />
 
         <p>
-          Sound Realms are very proud and excited to announce a collaboration with ACE Gamebooks and author Jonathan Green to create an audio adventure version of <i>Dracula, Curse of the Vampire</i> (2021) for the Sound Realms platform.
+          Sound Realms are very proud and excited to announce a collaboration with ACE Gamebooks and
+          author Jonathan Green to create an audio adventure version of
+          <i>Dracula, Curse of the Vampire</i> (2021) for the Sound Realms platform.
         </p>
         <p>
-Oscar Carlquist: "We are very pleased to present the next upcoming addition to Sound Realms and I can't wait to start creating some truly haunting and immersive soundscapes for <i>Dracula: Curse of the Vampire</i>. The interactive gamebook version of Bram Stoker's classic is masterfully written by Jon Green and taking this into the audio realm will be as much of an honour as a challenge that we here at Sound Realms are ready to embrace with enthusiasm and dedication!"
-</p>
+          Oscar Carlquist: "We are very pleased to present the next upcoming addition to Sound
+          Realms and I can't wait to start creating some truly haunting and immersive soundscapes
+          for <i>Dracula: Curse of the Vampire</i>. The interactive gamebook version of Bram
+          Stoker's classic is masterfully written by Jon Green and taking this into the audio realm
+          will be as much of an honour as a challenge that we here at Sound Realms are ready to
+          embrace with enthusiasm and dedication!"
+        </p>
         <p>
-Jonathan Green: "I am thrilled that Sound Realms are turning <i>Dracula: Curse of the Vampire</i> into the first audio ACE Gamebook and can't wait to hear how they bring the horror to life through the use of soundscapes and top-notch voice talent. It's going to be bloodcurdlingly brilliant!"
+          Jonathan Green: "I am thrilled that Sound Realms are turning
+          <i>Dracula: Curse of the Vampire</i> into the first audio ACE Gamebook and can't wait to
+          hear how they bring the horror to life through the use of soundscapes and top-notch voice
+          talent. It's going to be bloodcurdlingly brilliant!"
         </p>
 
+        <MyButton
+          external="true"
+          class="button"
+          href="https://www.kickstarter.com/projects/varghand/sound-realms-dracula-curse-of-the-vampire"
+        >
+          Join us on Kickstarter!
+        </MyButton>
       </div>
-      <img src="../../assets/dracula/dracula-download.png" class="image" />
 
       <YouTubeEmbed video="https://www.youtube.com/embed/Jrq3Hp6GXxY?si=P3WYmKPRIqgxzoGa" />
-
 
       <div class="strip">
         <h2>Sign up for the Newsletter</h2>
@@ -53,15 +68,16 @@ Jonathan Green: "I am thrilled that Sound Realms are turning <i>Dracula: Curse o
 </template>
 
 <script>
-import MainLayout from '../../layouts/Main.vue';
-import MainFooter from '../../components/MainFooter.vue';
-import TopMenu from '../../components/TopMenu.vue';
-import NewsletterSignupForm from '../../components/NewsletterSignupForm.vue';
-import YouTubeEmbed from '../../components/YoutubeEmbed.vue';
-import MyBreadcrumbs from '../../components/MyBreadcrumbs.vue';
+import MainLayout from "../../layouts/Main.vue";
+import MainFooter from "../../components/MainFooter.vue";
+import TopMenu from "../../components/TopMenu.vue";
+import NewsletterSignupForm from "../../components/NewsletterSignupForm.vue";
+import YouTubeEmbed from "../../components/YoutubeEmbed.vue";
+import MyBreadcrumbs from "../../components/MyBreadcrumbs.vue";
+import MyButton from '@/components/MyButton.vue';
 
 export default {
-  name: 'DraculaPage',
+  name: "DraculaPage",
   components: {
     MainLayout,
     MainFooter,
@@ -69,6 +85,7 @@ export default {
     NewsletterSignupForm,
     YouTubeEmbed,
     MyBreadcrumbs,
+    MyButton,
   },
 };
 </script>
